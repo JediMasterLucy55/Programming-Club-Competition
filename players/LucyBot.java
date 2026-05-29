7a177bfa6a798bc3e33403ec7c8fdf41a6eae720
JediMasterLucy <lucia.hans@icloud.com>
2026-05-29 14:48:17 -0500

    check()

--- players/LucyBot.java
+++ players/LucyBot.java
@@ -1,26 +1,33 @@
 package players;
 
-import gamefiles.botInterface;
 import gamefiles.readOnlyGameState;
 
 public class LucyBot implements gamefiles.botInterface {
+    int currentTokens;
     public int getBet(readOnlyGameState g) {
-        int currentTokens = g.getMyTokens();
+        currentTokens = g.getMyTokens();
         int opponentTokens = g.getOpponentTokens();
         int tokensToBetThisRound = 5;
 
-        if (opponentTokens < currentTokens) {
+        if (opponentTokens <= currentTokens) {
             tokensToBetThisRound = currentTokens;
+            boolean correct = betValid(tokensToBetThisRound);
+            if (!correct) {
+                tokensToBetThisRound = 5;
+            }
         } else if (opponentTokens > currentTokens) {
             if (g.getOpponentPreviousBet() > 5 && g.getOpponentPreviousBet() < currentTokens) {
                 tokensToBetThisRound = g.getOpponentPreviousBet() + 5;
             }
         }
 
-        if (g.getCurrentRound() == 0) {
-            return currentTokens;
-        }
-
         return tokensToBetThisRound;
+    }
+
+    private boolean betValid(int a) {
+        if (a < 5 || a > currentTokens) {
+            return false;
+        }
+        return true;
     }
 }
